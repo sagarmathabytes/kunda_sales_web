7c81301835aaec0ab22adf8b4d8afc68f405ea8b
RS-30000 <sagarmathabytes1@gmail.com>
2026-07-19 14:39:07 +0545

    chore: update obfuscated internal Dart runtime symbols and add NOTICES file

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2017197150" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "4025679231" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
